af1b78d1de4283ae10d82e9b668b0526fdd4e703
fbigiolli <felipebigiolli@gmail.com>
2023-09-02 13:51:51 -0300

    ej2 final

--- ej2.cpp
+++ ej2.cpp
@@ -5,30 +5,34 @@
 
 using namespace std;
 
+bool coef_comparison(pair<pair<long long,long long>,int> p1, pair<pair<long long,long long>,int> p2 ){
+    return ((p1.first.first * p2.first.second) < (p2.first.first * p1.first.second));
+}
+
 double mod = 1e9 + 7;
 
-long long parciales(vector<int>& tiempos, vector<int>& descontentos){
-    vector<pair<float,int>> factores(tiempos.size());
+long long parciales(vector<long long>& tiempos, vector<long long>& descontentos){
+    vector<pair<pair<long long,long long>,int>> factores(tiempos.size());
 
     // Armar el vector de coeficientes de descontentos
     for (int i = 0; i < tiempos.size(); i++) {
-        factores[i] = {descontentos[i] / tiempos[i], i};
+        factores[i] = {{descontentos[i] , tiempos[i]}, i};
     }
 
     // Ordenar el vector
-    sort(factores.begin(),factores.end());
+    sort(factores.begin(),factores.end(),coef_comparison);
 
     long long res = 0;
 
     // Definir una variable para acumular el tiempo transcurrido en la correccion
-    int tiempoAcumulado = 0;
+    long long tiempoAcumulado = 0;
     
     for (int i = factores.size() - 1; i >= 0; i--){
         // Obtener el indice de aquel que tiene mayor factor
         int indexActual = factores[i].second;
         // Obtener el tiempo necesario para corregir y el descontento por minuto
-        int tiempoDeCorreccion = tiempos[indexActual];
-        int descontento = descontentos[indexActual];
+        long long tiempoDeCorreccion = tiempos[indexActual];
+        long long descontento = descontentos[indexActual];
 
         // Aumentar el tiempo acumulado
         tiempoAcumulado += tiempoDeCorreccion;
@@ -51,8 +55,8 @@ int main() {
     for(int i = 0; i < test_cases_number; i++) {
         cin >> n;
 
-        vector<int> tiempos(n, 0);
-        vector<int> descontentos(n,0);
+        vector<long long> tiempos(n, 0);
+        vector<long long> descontentos(n,0);
 
         for(int j = 0; j < n; j++) {
             cin >> tiempos[j];
