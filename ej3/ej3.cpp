d637c1ddc340cc4b10bef5d75ffbc6543b9b662f
lucaveron <lucaver@hotmail.com>
2023-09-08 21:49:09 -0300

    ej3 corregido

--- ej3/ej3.cpp
+++ ej3/ej3.cpp
@@ -1,117 +1,162 @@
+
 #include <iostream>
 #include <vector>
+#include <map>
 
 using namespace std;
 
-bool encontrado = false;
-
-/*
-void afip(vector<int>& libro, vector<char>& signos, int index, int balanceFinal) {
-    // Caso base
-    if (index == libro.size()) {
-        if (balanceFinal == 0) {
-            for(char c : signos){
-                cout << c;
-            }
-            encontrado = true;
-            cout << endl;
-        }
-        return; 
-    }
-
-    // Recursión usando el signo ? para el actual
-    signos.push_back('?');
-    afip(libro, signos, index + 1, balanceFinal);
-    signos.pop_back();
-    
-    if (!encontrado) {
-        // Recursión usando el signo + para el actual
-        signos.push_back('+');
-        afip(libro, signos, index + 1, balanceFinal - libro[index]);
-        signos.pop_back();
-
-        // Recursión usando el signo - para el actual
-        signos.push_back('-');
-        afip(libro, signos, index + 1, balanceFinal + libro[index]);
-        signos.pop_back();
-
-    }
-}
-*/
-
 vector<char> res = {};
-
-bool sePuede(vector<int>& libro, vector<char>& signos, int index, int balanceFinal) {
-    // Caso base
-    if(index == libro.size()) {
-        if(balanceFinal == 0)
-            return true;
-    } else {
-        // Recursión usando el signo + para el actual
-        signos.push_back('+');
-        bool suma = sePuede(libro, signos, index + 1, balanceFinal - libro[index]);
-        signos.pop_back();
-
-        // Recursión usando el signo - para el actual
-        signos.push_back('-');
-        bool resta = sePuede(libro, signos, index + 1, balanceFinal + libro[index]);
-        signos.pop_back();
-
-        return suma || resta;
+int sumaElementos(vector<int>& s) {
+    int suma = 0;
+    for(int i = 0; i < s.size(); i++) {
+        suma += s[i];
     }
+    return suma;
 }
 
-void afip(vector<int>& libro, vector<char>& signos, int index, int balanceFinal) {
+void reconstruir(vector<int>& libro, vector<char>& signos, vector<vector<bool>>& m, int index, int balanceFinal) {
 
-    int balance = balanceFinal;
-    for(int i = 0; i < signos.size(); i++) {
-        signos[i] = '+';
-        bool suma = sePuede(libro, signos, i + 1, balance  - libro[index]);
 
-        signos[i] = '-';
-        bool resta = sePuede(libro, signos, i + 1, balance + libro[index]);
+    int offset =  m[0].size()/2 ;
 
-        if(suma && resta) 
-            signos[i] = '?';
-        else if(suma) {
+    for (int i = libro.size() - 1; i > 0; i--) {
+
+        bool valorResta = m[i - 1][balanceFinal - libro[i] + offset];
+        bool valorSuma = m[i - 1][balanceFinal + libro[i]  + offset];
+
+            if (valorResta) {
             signos[i] = '+';
-            balance -= libro[index];
+            balanceFinal-=libro[i];
         }
         else {
-            balance += libro[index];
             signos[i] = '-';
+            balanceFinal+=libro[i];
+        }
+
+    }
+
+    if(libro[0] > 0) {
+        signos[0] = '+';
+    } else
+        signos[0] = '-';
+}
+
+std::vector<int> eliminarRepetidosYObtenerIndices(std::vector<int> &originalVector) {
+    std::vector<int> duplicateIndices;
+    std::map<int, int> elementCount;
+
+    for (int i = 0; i < originalVector.size(); ++i) {
+        int element = originalVector[i];
+        if (elementCount.find(element) == elementCount.end()) {
+            // Este elemento no se ha encontrado antes
+            elementCount[element] = i; // Guardar el índice del elemento
+        } else {
+            // Este elemento se ha encontrado antes
+            if (elementCount[element] != -1) {
+                duplicateIndices.push_back(elementCount[element]); // Guardar el índice duplicado
+                elementCount[element] = -1; // Marcar como procesado
+            }
+            duplicateIndices.push_back(i); // Guardar el índice duplicado
         }
     }
+
+    // Modificar el vector original con los elementos únicos
+    std::vector<int> uniqueVector;
+    for (int element : originalVector) {
+        if (elementCount[element] != -1) {
+            uniqueVector.push_back(element);
+            elementCount[element] = -1; // Marcar como procesado
+        }
+    }
+
+    originalVector = uniqueVector;
+
+    return duplicateIndices;
+}
+
+
+void afip(vector<int>& libro, vector<char>& signos, vector<vector<bool>>& m, int index, int balanceFinal) {
+    if (libro.size() == 0) {
+        return;
+    }
+    int offset =  m[0].size()/2;
+
+    m[0][offset + libro[0]] = true;
+    m[0][offset - libro[0]] = true;
+
+    for (int i = 1; i < libro.size(); i++)
+    {
+        for (int j = 0; j < m[0].size() && j + libro[i] < offset * 2; j++)
+
+        {
+            bool valorIzquierda = false;
+            bool valorDerecha = false;
+            if (j - libro[i] >= 0)
+            {
+                valorIzquierda = m[i - 1][j - libro[i]];
+            }
+
+            if (j + libro[i] < offset * 2)
+            {
+                valorDerecha = m[i - 1][j + libro[i]];
+            }
+
+            m[i][j] = m[i][j] || valorDerecha || valorIzquierda ;
+        }
+    }
+/*
+    for(int i = 0; i < m.size(); i++) {
+        for(int j = 0; j < m[0].size(); j++) {
+            cout << m[i][j] << " ";
+        }
+        cout << endl;
+    }
+    */
+
+    reconstruir(libro, signos, m, index, balanceFinal + offset);
 }
 
 int main() {
-    
+
     int test_cases_number;
     int n;
     int balanceFinal;
 
     cin >> test_cases_number;
-    
+
     for(int i = 0; i < test_cases_number; i++) {
         cin >> n;
         cin >> balanceFinal;
         balanceFinal = balanceFinal / 100;
-                
+
         vector<int> libro(n);
-        vector<char> signos(n);
-        
+
+
         for (int i = 0; i < n; i++) {
             cin >> libro[i];
             libro[i] = libro[i] / 100;
         }
-        
-        afip(libro, signos, 0, balanceFinal);
+        vector<int> duplicados = eliminarRepetidosYObtenerIndices(libro);
+        vector<char> signos(libro.size());
 
+        int sizeMatriz = sumaElementos(libro);
+
+        vector<vector<bool>> m(libro.size(), vector<bool>(2 * sizeMatriz + 1, false));
+
+        afip(libro, signos, m, 0, balanceFinal);
+
+
+        for (int j = 0; j < duplicados.size(); ++j) {
+            signos.insert(signos.begin() + duplicados[j],'?');
+        }
+        cout << endl;
         for (int i = 0; i < n; i++) {
             cout << signos[i];
         }
+
+
         cout << endl;
-        encontrado = false;    
     }
     return 0;
 }
+
