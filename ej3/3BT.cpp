7b313a570660d2ea72b642133597a9fc144df543
Mateo <88727735+mateoschiro8@users.noreply.github.com>
2023-09-10 12:31:16 -0300

    BT mas clean (no pasa el juez)

--- ej3/3BT.cpp
+++ ej3/3BT.cpp
@@ -4,16 +4,19 @@
 
 using namespace std;
 
-int sumaRestantes(vector<int>& libro, int index) {
+int sumaRestantes(vector<int>& libro, int index, int ignorarElem) {
     int suma = 0;
-    for(int i = index; i < libro.size(); ++i)
-        suma += libro[i];
+    for(int i = index; i < libro.size(); ++i) {
+        if(i != ignorarElem)
+            suma += libro[i];
+    }
     return suma;
 }
 
-bool sePuede(vector<int>& libro, vector<char>& signos, int balanceFinal) {
-    // Caso base, ya llené todos los signos
-    if(signos.size() == libro.size()) {
+bool sePuede(vector<int>& libro, int index, int ignorarElem, int balanceFinal) {
+
+    // Caso base, ya llené consideré todos los elementos
+    if(index == libro.size()) {
         if(balanceFinal == 0)
             return true;
         else
@@ -21,65 +24,43 @@ bool sePuede(vector<int>& libro, vector<char>& signos, int balanceFinal) {
 
     } else {
 
-        // Suma de todos los elementos que todavía no tienen signo
-        int sumRestantes = sumaRestantes(libro, signos.size());
+        bool suma;
+        bool resta;
+        if (index != ignorarElem) {
 
-        // Si no tienen posibilidad de llegar al balance restante, corto antes
-        if(balanceFinal > 0 && sumRestantes < balanceFinal || balanceFinal < 0 && -sumRestantes > balanceFinal)
-            return false;
+            suma = sePuede(libro, index + 1, ignorarElem, balanceFinal - libro[index]);
+            resta = sePuede(libro, index + 1, ignorarElem, balanceFinal + libro[index]);
 
-        else {
+        } else {
 
-            // Recursión usando el signo + para el actual
-            signos.push_back('+');
-            bool suma = sePuede(libro, signos, balanceFinal - libro[signos.size() - 1]);
-            signos.pop_back();
+            suma = sePuede(libro, index + 1, ignorarElem, balanceFinal);
+            resta = sePuede(libro, index + 1, ignorarElem, balanceFinal);
 
-            // Recursión usando el signo - para el actual
-            signos.push_back('-');
-            bool resta = sePuede(libro, signos, balanceFinal + libro[signos.size() - 1]);
-            signos.pop_back();
-
-            return (suma || resta);
         }
+        return (suma || resta);
+
     }
 }
 
-void afip(vector<int>& libro, vector<char>& signos, int balanceFinal, set<int>& pendientes) {
+void afip(vector<int>& libro, vector<char>& signos, int balanceFinal) {
 
     int balance = balanceFinal;
 
     for(int i = 0; i < libro.size(); i++) {
 
-        // Si el valor del libro está en pendientes, lo saco, pongo un ? y aumento el balance
-        if(pendientes.count(libro[i])) {
-            pendientes.erase(libro[i]);
-            signos.push_back('?');
-            balance += libro[i];
-        } else {
-            signos.push_back('+');
-            bool suma = sePuede(libro, signos, balance - libro[i]);
-            signos.pop_back();
+            bool suma = sePuede(libro, 0, i, balance - libro[i]);
 
-            signos.push_back('-');
-            bool resta = sePuede(libro, signos, balance + libro[i]);
-            signos.pop_back();
+            bool resta = sePuede(libro, 0, i, balance + libro[i]);
 
-            // Si puedo llegar por ambos caminos, lo agrego a pendientes y disminuye el balance
-            if(suma && resta) {
-                signos.push_back('?');
-                pendientes.insert(libro[i]);
-                balance -= libro[i];
-            }
-            else if(suma) {
-                signos.push_back('+');
-                balance -= libro[i];
-            } else {
-                signos.push_back('-');
-                balance += libro[i];
-            }
+            if(suma && resta)
+                signos[i] = '?';
 
-        }
+            else if(suma)
+                signos[i] = '+';
+
+            else
+                signos[i] = '-';
+
     }
 }
 
@@ -97,15 +78,15 @@ int main() {
         balanceFinal = balanceFinal / 100;
                 
         vector<int> libro(n);
-        vector<char> signos = {};
-        set<int> pendientes = {};
+        vector<char> signos(n);
+
         
         for (int i = 0; i < n; i++) {
             cin >> libro[i];
             libro[i] = libro[i] / 100;
         }
         
-        afip(libro, signos, balanceFinal, pendientes);
+        afip(libro, signos, balanceFinal);
 
         for (int i = 0; i < n; i++) {
             cout << signos[i];
