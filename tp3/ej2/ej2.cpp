9204455e37d58954269ba1554933d41f61e34de8
fbigiolli <felipebigiolli@gmail.com>
2023-11-07 16:00:21 -0300

    tp3 terminado

--- tp3/ej2/ej2.cpp
+++ tp3/ej2/ej2.cpp
@@ -9,22 +9,22 @@ void floydWarshall(vector<vector<int>>& latencias) {
     for(int k = 0; k < latencias.size(); k++) {
         for(int i = 0; i < latencias.size(); i++) {
             for(int j = 0; j < latencias.size(); j++) {
-                
+
                 latencias[i][j] = min(latencias[i][j], latencias[i][k] + latencias[k][j]);
-                
+
             }
         }
     }
 }
 
 bool esFloydWarshall(vector<vector<int>>& latencias) {
-    
-    for(int i = 0; i < latencias.size(); i++) {
-        for(int j = 0; j < latencias.size(); j++) {
-            for(int k = 0; k < latencias.size(); k++) {
+
+    for(int k = 0; k < latencias.size(); k++) {
+        for(int i = 0; i < latencias.size(); i++) {
+            for(int j = 0; j < latencias.size(); j++) {
                 // Si se cumple esta condicion, entonces hay un camino distinto con menor latencia y por lo tanto la matriz no es valida
-                if (latencias[j][k] > latencias[j][i] + latencias[i][k])
-                    return false; 
+                if (latencias[i][j] > latencias[i][k] + latencias[k][j])
+                    return false;
             }
         }
     }
@@ -36,7 +36,7 @@ void recibirParametros(vector<vector<int>>& latencias){
     // Arma la matriz llenando simetricamente la parte de arriba de la diagonal de 0's con los inputs.
     for(int i = 1; i < latencias.size(); i++) {
         for(int j = 0; j < i; j++) {
-            int lat; 
+            int lat;
             cin >> lat;
             latencias[i][j] = lat;
             latencias[j][i] = lat;
@@ -46,24 +46,24 @@ void recibirParametros(vector<vector<int>>& latencias){
 
 void reconstruirYPrintearDistancias(vector<vector<int>>& latencias) {
     // Ponemos todas las aristas posibles con los pesos de la matriz de FW.
-   for(int i = 0; i < latencias.size(); i++) {
-        for(int j = 0; j < latencias.size(); j++) {
-            for(int k = 0; k < latencias.size(); k++) {
+    for(int k = 0; k < latencias.size(); k++) {
+        for(int i = 0; i < latencias.size(); i++) {
+            for(int j = 0; j < latencias.size(); j++) {
                 // Si se cumple esta condicion, entonces hay un camino distinto con menor latencia y por lo tanto la arista no es necesaria
-                if (latencias[j][k] == latencias[j][i] + latencias[i][k] && (j != i && i != k)) // nos fijamos que j sea distinto de i, e i distinto de k para no caer en distancias de un nodo a si mismo
-                    latencias[j][k] = -1;
+                if (latencias[i][j] == latencias[i][k] + latencias[k][j] && k!=i && k!=j) // nos fijamos que j sea distinto de k, e i distinto de k para no caer en distancias de un nodo a si mismo
+                    latencias[i][j] = inf;
             }
         }
     }
-    
+
     // Ponemos en inf todas las posiciones que no nos sirven revisar y en 1 las que si.
     for (int i = 0; i < latencias.size(); i++) {
         for (int j = 0; j < latencias.size(); j++) {
-            if(latencias[i][j] == -1)
+            if(latencias[i][j] == inf)
                 latencias[i][j] = inf;
             else if(i != j)
                 latencias[i][j] = 1;
-            
+
         }
     }
 
@@ -77,8 +77,6 @@ void reconstruirYPrintearDistancias(vector<vector<int>>& latencias) {
         }
         cout << endl;
     }
-    
-    // cout << endl;
 
 }
 
@@ -88,23 +86,21 @@ int main() {
 
     int cantTests;
     cin >> cantTests;
-    
+
     for (int i = 0; i < cantTests; i++) {
-        
+
         int cantPCs;
         cin >> cantPCs;
         vector<vector<int>> latencias(cantPCs, vector<int>(cantPCs, 0));
         recibirParametros(latencias);
-        
-        bool res = esFloydWarshall(latencias);
 
         if(esFloydWarshall(latencias)) {
             cout << "POSIBLE" << endl;
             reconstruirYPrintearDistancias(latencias);
         }
-        else 
+        else
             cout << "IMPOSIBLE" << endl;
     }
-    
+
     return 0;
 }
